4679ed2531ff4bd0f2708689635a995f8fd338f8
claytonjschneider <claytonjschneider@gmail.com>
2022-01-10 13:12:01 -0600

    syncing

--- scripts/convertNirsDS.m
+++ scripts/convertNirsDS.m
@@ -1,23 +1,15 @@
-% classdef convertNirsDS
-%     methods (Static)
-%         function res = convertNirsDS(path)
-%             NirsPaths = dir(fullfile(path, '**', '*_fNIRS'));
-% 
-%             for i = 1:numel(NirsPaths)
-%                 fpath = strcat(NirsPaths(i).folder, '/', NirsPaths(i).name);
-%                 HomerOfflineConverter(fpath)
-%             end
-%             
-%             res = 1
-%         end
-%     end
-% end
-%     
+% this script runs the HomerOfflineConverter (which is stored
+% next to it) on all of the child folders, of any path depth,
+% from the supplied path where the child folder name ends in 
+% _fNIRS. It could be modified to handle other use cases as well,
+% but this is sufficient for the conversion of the CARE 
+% fNIRS dataset. 
+
 
 path = "/data/perlman/moochie/study_data/CARE/NIRS_data_preproc/";
 
 NirsPaths = dir(fullfile(path, '**', '*_fNIRS'));
-
+%%
 for i = 1:numel(NirsPaths)
     fpath = strcat(NirsPaths(i).folder, '/', NirsPaths(i).name);
     HomerOfflineConverter(fpath)
