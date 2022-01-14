1254181c709006fcb5b0c7c613e40360edcd29e3
claytonjschneider <claytonjschneider@gmail.com>
2022-01-14 14:27:00 -0600

    CNDA download script

--- CNDA_Downloads/downloadCNDASession2.sh
+++ CNDA_Downloads/downloadCNDASession2.sh
@@ -14,30 +14,31 @@ CURR_DIR=`pwd`
 JSESSION=`curl -u ${USER} "${SITE}/data/JSESSION"`
 
 #!! Update this with directory where sessions should be stored.
-SESSIONS_DIR=./CNDA_Sessions
-if [ ! -d ${SESSIONS_DIR} ]; then
-   mkdir ${SESSIONS_DIR}
-fi
+SESSIONS_DIR=./CNDA_download_repo
+# if [ ! -d ${SESSIONS_DIR} ]; then
+#    mkdir ${SESSIONS_DIR}
+# fi
 
-let i=0
+let i=1
 while read line
 do
     let i=$i+1
     echo Downloading ${i} ${line}
-    proj=`echo $line | cut -d, -f1`
-    subj=`echo $line | cut -d, -f2`
-    exp=`echo $line | cut -d, -f3`
+    proj=`echo $line | cut -d, -f2`
+    subj=`echo $line | cut -d, -f3`
+    exp=`echo $line | cut -d, -f4`
     # Option 1: Download all files in the session
     curl -b JSESSIONID=${JSESSION} "${SITE}/data/archive/projects/${proj}/subjects/${subj}/experiments/${exp}/scans/*/files?format=zip" > ${SESSIONS_DIR}/${exp}.zip
     # Option 2: Download specific scans by name (be sure to encode spaces as %20)
     # curl -b JSESSIONID=${JSESSION} "${SITE}/data/archive/projects/${proj}/subjects/${subj}/experiments/${exp}/scans/MPRAGE%20GRAPPA2/files?format=zip" > ${SESSIONS_DIR}/${exp}.zip
     # Option 3: Download specific scans by number (comma-separated for multiple scans)
     # curl -b JSESSIONID=${JSESSION} "${SITE}/data/archive/projects/${proj}/subjects/${subj}/experiments/${exp}/scans/2,3/files?format=zip" > ${SESSIONS_DIR}/${exp}.zip
+
     #!! If you don't want script to unzip sessions and remove zip file for you, comment lines below
-    cd ${SESSIONS_DIR}
-    unzip ${exp}.zip
-    rm ${exp}.zip
-    cd ${CURR_DIR}
+    # cd ${SESSIONS_DIR}
+    # unzip ${exp}.zip
+    # rm ${exp}.zip
+    # cd ${CURR_DIR}
     #!! End commentable block
 done < $INPUT_FILE
 
