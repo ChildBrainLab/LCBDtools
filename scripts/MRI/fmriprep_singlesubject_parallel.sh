47df5cb003ea8cdd88552594a5b801a8b90e6f0c
claytonjschneider <claytonjschneider@gmail.com>
2022-04-20 09:44:12 -0500

    lots of changes to fmriprep script. parallelization seems to be working - running into seg fault issues due to zero-sized template files from templateflow, and fixed by setting environment variables in the docker containers and passing custom templateflow directory.

---
+++ scripts/MRI/fmriprep_singlesubject_parallel.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# usage: ./fmriprep.sh </path/to/BIDS/dataset> ... optional: <participant-to-resume-with>
+
+# source /data/perlman/moochie/resources/server_access/MRIenv/bin/activate.csh
+
+export FREESURFER_HOME="/usr/local/pkg/freesurfer"
+
+for sub in $(cat $(dirname $1)/parallel_subs.txt)
+do
+	#docker run --rm\
+	docker run --rm -u $( id -u)\
+		--name FMRIPREP_$(basename $(dirname $1))_$sub \
+		-v $1:/data:ro \
+		-v $1/derivatives:/out \
+		-v $1/work:/work \
+		-v $(dirname $1)/bids-database:/bids-database \
+		-v $FREESURFER_HOME/license.txt:/opt/freesurfer/license.txt \
+		-v /data/perlman/moochie/resources/templateflow:/templateflow \
+		-e TEMPLATEFLOW_HOME='/templateflow' \
+		nipreps/fmriprep:latest \
+		/data /out/fmriprep \
+		participant \
+		-w /work \
+		--longitudinal \
+		--skip_bids_validation \
+        	--use-aroma \
+		--nthreads 16 \
+		--low-mem \
+		--mem-mb 15000 \
+		--output-spaces MNIPediatricAsym:cohort-2:res-native \
+		--participant-label $sub &
+
+	# docker system prune -f
+done
+
+
