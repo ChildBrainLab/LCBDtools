9ad1b36e93b36b96f16f8c332b876f8568f641a8
Clayton Schneider <claytonjschneider@gmail.com>
2022-05-26 12:00:14 -0500

    actual single subject shell scripts

---
+++ scripts/MRI/fmriprep_ss.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# usage: ./fmriprep.sh </path/to/BIDS/dataset> <subject>
+
+# source /data/perlman/moochie/resources/server_access/MRIenv/bin/activate.csh
+
+export FREESURFER_HOME="/usr/local/pkg/freesurfer"
+
+sub=$2
+
+docker run --rm -u $( id -u)\
+	--name FMRIPREP_$(basename $(dirname $1))_$sub \
+	-v $1:/data:ro \
+	-v $1/derivatives:/out \
+	-v $1/work:/work \
+	-v $(dirname $1)/bids-database:/bids-database \
+	-v $FREESURFER_HOME/license.txt:/opt/freesurfer/license.txt \
+	-v /data/perlman/moochie/resources/templateflow:/home/fmriprep/.cache/templateflow \
+	-e TEMPLATEFLOW_HOME='/home/fmriprep/.cache/templateflow' \
+	nipreps/fmriprep:latest \
+	/data /out/fmriprep \
+	participant \
+	-w /work \
+	--longitudinal \
+	--skip_bids_validation \
+       	--use-aroma \
+	--nthreads 16 \
+	--low-mem \
+	--mem-mb 15000 \
+	--output-spaces MNIPediatricAsym:cohort-2:res-2 \
+	--participant-label $sub
+
+
