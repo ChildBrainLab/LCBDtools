590c7ae0abfc7e32c6e205f8b1fec9ba74afe6ba
Denny Schaedig <desc7849@colorado.edu>
2023-05-03 13:54:42 -0500

    Fixed subject counter script to also count dyads

--- scripts/DataManagement/subject_counter.py
+++ scripts/DataManagement/subject_counter.py
@@ -1,23 +1,32 @@
 
 import os, sys
 
-data_path = '../../../../analysis/CARE/MRI_data_clean_2/'
 
 class counter:
-    def __init__(self, path):
-        self.data_path = path
-    
+    def __init__(self, path = None):
+        if path != None:
+            self.data_path = path
+        else:
+            self.data_path = '../../../../analysis/CARE/NIRS_data_clean_2/'
+
     def count(self):
         self.subject_count = {}
         subjects = os.listdir(self.data_path)
         for subject in subjects:
-            if os.path.isdir() == False:
+            if os.path.isdir(self.data_path + subject) == False:
                 continue    
-            sessions = os.listdir()
+            sessions = os.listdir(self.data_path + subject)
             for session in sessions:
                 if session not in self.subject_count.keys():
                     self.subject_count[session] = []
                 self.subject_count[session].append(subject)
                         
     def count_dyads(self):
+        self.dyad_count = {}
+        for session in self.subject_count.keys():
+            found = []
+            for subject in self.subject_count[session]:
+                if subject[:5] not in found:
+                    found.append(subject[:5])
+            self.dyad_count[session] = found       
 
