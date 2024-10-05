e714cafecf16aa2c26e2b0ca80934cb25b71ca60
Schaedig <dennys@compute1-client-1.ris.wustl.edu>
2024-10-04 22:58:09 -0500

    Adjusted sandbox configuration to accomodate larger training sets

--- scripts/MRI/bsub/launch_sandbox.sh
+++ scripts/MRI/bsub/launch_sandbox.sh
@@ -2,4 +2,4 @@
 
 export LSF_DOCKER_VOLUMES="/storage1/fs1/perlmansusan/Active:/storage1/fs1/perlmansusan/Active /scratch1/fs1/perlmansusan:/scratch1/fs1/perlmansusan $HOME:$HOME"
 
-bsub -G compute-perlmansusan -J $USER-sandbox -a 'docker(continuumio/anaconda3:latest)' -Is -n 16 -R 'rusage[mem=26GB] select[mem>25000 && tmp>250]' /bin/bash 
+bsub -G compute-perlmansusan -J $USER-sandbox -a 'docker(continuumio/anaconda3:latest)' -Is -n 16 -R 'rusage[mem=50GB] select[mem>50000 && tmp>250]' /bin/bash 
