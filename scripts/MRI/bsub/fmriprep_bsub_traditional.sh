ecab62a834b35008506512cce0b87bef56030a87
Schaedig <dennys@compute1-client-1.ris.wustl.edu>
2024-11-08 10:17:05 -0600

    Updated fmriprep pipeline to fix new issue with job ID not being saved to execution environment

--- scripts/MRI/bsub/fmriprep_bsub_traditional.sh
+++ scripts/MRI/bsub/fmriprep_bsub_traditional.sh
@@ -6,14 +6,17 @@
 #BSUB -n 16
 #BSUB -R 'rusage[mem=30GB] select[mem>30000 && tmp>250]'
 
-cd /tmp/$JOBID.tmpdir
+JOBFOLDER=`ls /tmp/`
+HOMEDIR=`ls /home/`
 
-mkdir /tmp/$JOBID.tmpdir/scratch
-mkdir /tmp/$JOBID.tmpdir/output
+cd /tmp/$JOBFOLDER/
 
-/opt/conda/envs/fmriprep/bin/fmriprep /input /tmp/$JOBID.tmpdir/output participant --participant-label sub-$SUBJECT -w /tmp/$JOBID.tmpdir/scratch --fs-license-file /freesurfer/license.txt --output-spaces MNI152NLin6Asym:res-2 MNI152NLin2009cAsym:res-2 MNIPediatricAsym:cohort-2:res-2 --n_cpus 16 --mem 24 --low-mem --skip-bids-validation 
+mkdir /tmp/$JOBFOLDER/scratch
+mkdir /tmp/$JOBFOLDER/output
 
-cp -r /tmp/$JOBID.tmpdir/output/. /output/
+/opt/conda/envs/fmriprep/bin/fmriprep /input /tmp/$JOBFOLDER/output participant --participant-label sub-$SUBJECT -w /tmp/$JOBFOLDER/scratch --fs-license-file /freesurfer/license.txt --output-spaces MNI152NLin6Asym:res-2 MNI152NLin2009cAsym:res-2 MNIPediatricAsym:cohort-2:res-2 --n_cpus 16 --mem 24 --low-mem --skip-bids-validation 
+
+cp -r /tmp/$JOBFOLDER/output/. /output/
 
 for file in `find /output/ -name *$SUBJECT*.gz` ; do
     gzip -d $file
