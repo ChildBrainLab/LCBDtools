367e6ee203500f01cce6e61abac0185a768a8f5c
Schaedig <dennys@compute1-exec-132.ris.wustl.edu>
2025-01-10 19:07:25 +0000

    Adjust fieldmap image03 to resolve field of view issue

--- scripts/DataManagement/nda_upload.py
+++ scripts/DataManagement/nda_upload.py
@@ -1,163 +1,4 @@
 
-
-image03_template_filepath = '/storage1/fs1/perlmansusan/Active/moochie/study_data/CARE/CARE_image03.csv'
-
-bids_folder = '/storage1/fs1/perlmansusan/Active/moochie/analysis/CARE/ME_MRI_data/'
-
-experiment_ids = {
-    'AHKJ Task Episode A': 1890,
-    'AHKJ Task Episode B': 1891,
-    'AHKJ Task Episode C': 1892
-}
-
-experiment_files = {
-    'fMRI': '_echo-1_bold.nii',
-    'anatomical': '_T1w.nii',
-    'DWI': '_dir-AP_dwi.nii'
-}
-
-fmri_image03_json  = {  
-    'subjectkey' : None,
-    'src_subject_id': None,
-    'interview_date': None,
-    'interview_age': None,
-    'sex': None,
-    'comments_misc': None,
-    'image_file': None,
-    'image_description': 'fMRI',
-    'experiment_id': None,
-    'scan_type': "fMRI",
-    'scan_object': "Live",
-    'image_file_format': "NIFTI",
-    'image_modality': "MRI",
-    'scanner_manufacturer_pd': "Siemens",
-    'scanner_type_pd': "Magnetom Prisma",
-    'scanner_software_versions_pd': None,
-    'magnetic_field_strength': "3.0",
-    'mri_repetition_time_pd': 0.8,
-    'mri_echo_time_pd': "0.8",
-    'flip_angle': "7.0",
-    'acquisition_matrix': "[68, 68, 40]",
-    'mri_field_of_view_pd': "213 x 208 x 213.0 mm",
-    'patient_position': "Laying down face up",
-    'transformation_performed': "No",
-    'image_num_dimensions': 4,
-    'image_extent1': 68,
-    'image_extent2': 68,
-    'image_extent3': 40,
-    'image_extent4': None,
-    'extent4_type': 'Time',
-    'image_unit1': 'a.u.',
-    'software_preproc': 'BIDSkit 3.0.2',
-    'study': 'Biological Substrates of Maladaptive Stress Response in Early Childhood',
-    'experiment_description': None,
-    'visnum': None
-    }
-
-structural_image03_json  = {  
-    'subjectkey' : None,
-    'src_subject_id': None,
-    'interview_date': None,
-    'interview_age': None,
-    'sex': None,
-    'comments_misc': None,
-    'image_file': None,
-    'image_description': 'MPRAGE structural',
-    'experiment_id': None,
-    'scan_type': "MR structural",
-    'scan_object': "Live",
-    'image_file_format': "NIFTI",
-    'image_modality': "MRI",
-    'scanner_manufacturer_pd': "Siemens",
-    'scanner_type_pd': "Magnetom Prisma",
-    'scanner_software_versions_pd': None,
-    'magnetic_field_strength': "3.0",
-    'mri_repetition_time_pd': 0.8,
-    'mri_echo_time_pd': "0.8",
-    'flip_angle': "7.0",
-    'acquisition_matrix': "[  3 240 208 256   1   1   1   1]",
-    'mri_field_of_view_pd': "199.7 x 208 x 213.0 mm",
-    'patient_position': "Laying down face up",
-    'transformation_performed': "No",
-    'image_num_dimensions': 4,
-    'image_extent1': 240,
-    'image_extent2': 208,
-    'image_extent3': 256,
-    'image_extent4': None,
-    'extent4_type': 'Time',
-    'image_unit1': 'signal intensity',
-    'software_preproc': 'BIDSkit 3.0.2',
-    'study': 'Biological Substrates of Maladaptive Stress Response in Early Childhood',
-    'experiment_description': None,
-    'visnum': None
-    }
-
-dti_image03_json  = {  
-    'subjectkey' : None,
-    'src_subject_id': None,
-    'interview_date': None,
-    'interview_age': None,
-    'sex': None,
-    'comments_misc': None,
-    'image_file': None,
-    'image_description': 'DWI',
-    'experiment_id': None,
-    'scan_type': "MR diffusion",
-    'scan_object': "Live",
-    'image_file_format': "NIFTI",
-    'image_modality': "MRI",
-    'scanner_manufacturer_pd': "Siemens",
-    'scanner_type_pd': "Magnetom Prisma",
-    'scanner_software_versions_pd': None,
-    'magnetic_field_strength': "3.0",
-    'mri_repetition_time_pd': 0.8,
-    'mri_echo_time_pd': "0.8",
-    'flip_angle': "7.0",
-    'acquisition_matrix': "[  4 140 140  92  99   1   1   1]",
-    'mri_field_of_view_pd': "210 x 210 x 138 x 319.7 mm",
-    'patient_position': "Laying down face up",
-    'transformation_performed': "No",
-    'image_num_dimensions': 4,
-    'image_extent1': 140,
-    'image_extent2': 140,
-    'image_extent3': 92,
-    'image_extent4': 99,
-    'extent4_type': '',
-    'image_unit1': 'signal intensity',
-    'software_preproc': 'BIDSkit 3.0.2',
-    'study': 'Biological Substrates of Maladaptive Stress Response in Early Childhood',
-    'experiment_description': None,
-    'bvek_bval_files': None,
-    'bvecfile': None,
-    'bvalfile': None,
-    'visnum': None
-    }
-
-# Create an image03 for all data in upload
-
-# For each subject
-
-    # Grab subject level needed info
-
-    # For each session
-
-        # Grab session level needed info
-
-        # Find movie version
-
-        # Find experiment ID
-
-        # For each scan
-
-        # For functional image
-
-        # Specify the experiment ID
-
-        # Grab number of volumes to set 'image_extent4'
-
-        #
-
-
 image03_template  = {  
     'subjectkey' : None,
     'src_subject_id': None,
@@ -227,3 +68,275 @@ image03_template  = {
     'visnum': None,
     'accession_number': None
     }
+
+fmri_bold_image03_json  = {  
+    'subjectkey' : None,
+    'src_subject_id': None,
+    'interview_date': None,
+    'interview_age': None,
+    'sex': None,
+    'comments_misc': None,
+    'image_file': None,
+    'image_description': 'fMRI',
+    'experiment_id': None,
+    'scan_type': "fMRI",
+    'scan_object': "Live",
+    'image_file_format': "NIFTI",
+    'image_modality': "MRI",
+    'scanner_manufacturer_pd': "Siemens",
+    'scanner_type_pd': "Magnetom Prisma",
+    'scanner_software_versions_pd': None,
+    'magnetic_field_strength': "3.0",
+    'mri_repetition_time_pd': 0.8,
+    'mri_echo_time_pd': "0.8",
+    'flip_angle': "7.0",
+    'acquisition_matrix': "[68, 68, 40]",
+    'mri_field_of_view_pd': "213 x 208 x 213.0 mm",
+    'patient_position': "Laying down face up",
+    'transformation_performed': "No",
+    'image_num_dimensions': 4,
+    'image_extent1': 68,
+    'image_extent2': 68,
+    'image_extent3': 40,
+    'image_extent4': None,
+    'extent4_type': 'Time',
+    'image_unit1': 'a.u.',
+    'software_preproc': 'BIDSkit 3.0.2',
+    'study': 'Biological Substrates of Maladaptive Stress Response in Early Childhood',
+    'experiment_description': None,
+    'visnum': None
+    }
+
+structural_image03_json  = {  
+    'subjectkey' : None,
+    'src_subject_id': None,
+    'interview_date': None,
+    'interview_age': None,
+    'sex': None,
+    'comments_misc': None,
+    'image_file': None,
+    'image_description': 'MPRAGE structural',
+    'experiment_id': None,
+    'scan_type': "MR structural",
+    'scan_object': "Live",
+    'image_file_format': "NIFTI",
+    'image_modality': "MRI",
+    'scanner_manufacturer_pd': "Siemens",
+    'scanner_type_pd': "Magnetom Prisma",
+    'scanner_software_versions_pd': None,
+    'magnetic_field_strength': "3.0",
+    'mri_repetition_time_pd': 0.8,
+    'mri_echo_time_pd': "0.8",
+    'flip_angle': "7.0",
+    'acquisition_matrix': "[  3 240 208 256   1   1   1   1]",
+    'mri_field_of_view_pd': "199.7 x 208 x 213.0 mm",
+    'patient_position': "Laying down face up",
+    'transformation_performed': "No",
+    'image_num_dimensions': 3,
+    'image_extent1': 240,
+    'image_extent2': 208,
+    'image_extent3': 256,
+    'image_unit1': 'signal intensity',
+    'software_preproc': 'BIDSkit 3.0.2',
+    'study': 'Biological Substrates of Maladaptive Stress Response in Early Childhood',
+    'experiment_description': None,
+    'visnum': None
+    }
+
+dwi_image03_json  = {  
+    'subjectkey' : None,
+    'src_subject_id': None,
+    'interview_date': None,
+    'interview_age': None,
+    'sex': None,
+    'comments_misc': None,
+    'image_file': None,
+    'image_description': 'DWI',
+    'experiment_id': None,
+    'scan_type': "MR diffusion",
+    'scan_object': "Live",
+    'image_file_format': "NIFTI",
+    'image_modality': "MRI",
+    'scanner_manufacturer_pd': "Siemens",
+    'scanner_type_pd': "Magnetom Prisma",
+    'scanner_software_versions_pd': None,
+    'magnetic_field_strength': "3.0",
+    'mri_repetition_time_pd': 0.8,
+    'mri_echo_time_pd': "0.8",
+    'flip_angle': "7.0",
+    'acquisition_matrix': "[  4 140 140  92  99   1   1   1]",
+    'mri_field_of_view_pd': "210 x 210 x 138 x 319.7 mm",
+    'patient_position': "Laying down face up",
+    'transformation_performed': "No",
+    'image_num_dimensions': 4,
+    'image_extent1': 140,
+    'image_extent2': 140,
+    'image_extent3': 92,
+    'image_extent4': 99,
+    'extent4_type': '',
+    'image_unit1': 'signal intensity',
+    'software_preproc': 'BIDSkit 3.0.2',
+    'study': 'Biological Substrates of Maladaptive Stress Response in Early Childhood',
+    'experiment_description': None,
+    'bvek_bval_files': None,
+    'bvecfile': None,
+    'bvalfile': None,
+    'visnum': None
+    }
+
+fmap_image03_json  = {  
+    'subjectkey' : None,
+    'src_subject_id': None,
+    'interview_date': None,
+    'interview_age': None,
+    'sex': None,
+    'comments_misc': None,
+    'image_file': None,
+    'image_description': 'Field Map',
+    'experiment_id': None,
+    'scan_type': "Field Map",
+    'scan_object': "Live",
+    'image_file_format': "NIFTI",
+    'image_modality': "MRI",
+    'scanner_manufacturer_pd': "Siemens",
+    'scanner_type_pd': "Magnetom Prisma",
+    'scanner_software_versions_pd': None,
+    'magnetic_field_strength': "3.0",
+    'mri_repetition_time_pd': 0.8,
+    'mri_echo_time_pd': "0.8",
+    'flip_angle': "7.0",
+    'acquisition_matrix': "[ 4 68 68 40  3  1  1  1]",
+    'mri_field_of_view_pd': "204 x 204 x 120 mm",
+    'patient_position': "Laying down face up",
+    'transformation_performed': "BIDSified",
+    'image_num_dimensions': 3,
+    'image_extent1': 68,
+    'image_extent2': 68,
+    'image_extent3': 40,
+    'image_unit1': 'Tesla',
+    'software_preproc': 'BIDSkit 3.0.2',
+    'study': 'Biological Substrates of Maladaptive Stress Response in Early Childhood',
+    'experiment_description': None,
+    'visnum': None
+    }
+
+image03_template_filepath = '/storage1/fs1/perlmansusan/Active/moochie/study_data/CARE/CARE_image03.csv'
+
+bids_folder = '/storage1/fs1/perlmansusan/Active/moochie/analysis/CARE/ME_MRI_data/'
+
+experiments = [
+'anatomical',
+'AP_dwi',
+'PA_dwi',
+'AP_fmap',
+'PA_fmap',
+'movieA_echo-1_bold'
+'movieA_echo-2_bold',
+'movieA_echo-3_bold',
+'movieA_echo-2_sbref',
+'movieA_echo-3_sbref',
+'movieB_echo-1_bold',
+'movieB_echo-2_bold',
+'movieB_echo-3_bold',
+'movieB_echo-2_sbref',
+'movieB_echo-3_sbref',
+'movieC_echo-1_bold',
+'movieC_echo-2_bold',
+'movieC_echo-3_bold',
+'movieC_echo-2_sbref',
+'movieC_echo-3_sbref'
+]
+
+experiment_files = {
+'anatomical': '_T1w.nii.gz',
+'AP_dwi': '_dir-AP_dwi.nii.gz',
+'PA_dwi': '_dir-PA_dwi.nii.gz',
+'PA_dwi_sbref': '_dir-PA_sbref.nii.gz',
+'AP_fmap': '_dir-AP_epi.nii.gz',
+'PA_fmap': '_dir-PA_epi.nii.gz',
+'movieA_echo-1_bold': '_task-movieA_echo-1_bold.nii.gz',
+'movieA_echo-2_bold': '_task-movieA_echo-2_bold.nii.gz',
+'movieA_echo-3_bold': '_task-movieA_echo-3_bold.nii.gz',
+'movieA_echo-2_sbref': '_task-movieA_dir-PA_echo-2_sbref.nii.gz',
+'movieA_echo-3_sbref': '_task-movieA_dir-PA_echo-3_sbref.nii.gz',
+'movieB_echo-1_bold': '_task-movieB_echo-1_bold.nii.gz',
+'movieB_echo-2_bold': '_task-movieB_echo-2_bold.nii.gz',
+'movieB_echo-3_bold': '_task-movieB_echo-3_bold.nii.gz',
+'movieB_echo-2_sbref': '_task-movieB_dir-PA_echo-2_sbref.nii.gz',
+'movieB_echo-3_sbref': '_task-movieB_dir-PA_echo-3_sbref.nii.gz',
+'movieC_echo-1_bold': '_task-movieC_echo-1_bold.nii.gz',
+'movieC_echo-2_bold': '_task-movieC_echo-2_bold.nii.gz',
+'movieC_echo-3_bold': '_task-movieC_echo-3_bold.nii.gz',
+'movieC_echo-2_sbref': '_task-movieC_dir-PA_echo-2_sbref.nii.gz',
+'movieC_echo-3_sbref': '_task-movieC_dir-PA_echo-3_sbref.nii.gz'
+}
+
+experiment_ids = {
+    'anatomical': 0,
+    'AP_dwi': 0,
+    'PA_dwi': 0,
+    'AP_fmap': 0,
+    'PA_fmap': 0,
+    'movieA_echo-1_bold': 1890,
+    'movieA_echo-2_bold': 1890,
+    'movieA_echo-3_bold': 1890,
+    'movieA_echo-2_sbref': 1890,
+    'movieA_echo-3_sbref': 1890,
+    'movieB_echo-1_bold': 1891,
+    'movieB_echo-2_bold': 1891,
+    'movieB_echo-3_bold': 1891,
+    'movieB_echo-2_sbref': 1891,
+    'movieB_echo-3_sbref': 1891,
+    'movieC_echo-1_bold': 1892,
+    'movieC_echo-2_bold': 1892,
+    'movieC_echo-3_bold': 1892,
+    'movieC_echo-2_sbref': 1892,
+    'movieC_echo-3_sbref': 1892,
+}
+
+experiment_image03s = {
+    'anatomical': structural_image03_json,
+    'AP_dwi': dwi_image03_json,
+    'PA_dwi': dwi_image03_json,
+    'AP_fmap': fmap_image03_json,
+    'PA_fmap': fmap_image03_json,
+    'movieA_echo-1_bold': fmri_bold_image03_json,
+    'movieA_echo-2_bold': fmri_bold_image03_json,
+    'movieA_echo-3_bold': fmri_bold_image03_json,
+    'movieA_echo-2_sbref': fmri_bold_image03_json,
+    'movieA_echo-3_sbref': fmri_bold_image03_json,
+    'movieB_echo-1_bold': fmri_bold_image03_json,
+    'movieB_echo-2_bold': fmri_bold_image03_json,
+    'movieB_echo-3_bold': fmri_bold_image03_json,
+    'movieB_echo-2_sbref': fmri_bold_image03_json,
+    'movieB_echo-3_sbref': fmri_bold_image03_json,
+    'movieC_echo-1_bold': fmri_bold_image03_json,
+    'movieC_echo-2_bold': fmri_bold_image03_json,
+    'movieC_echo-3_bold': fmri_bold_image03_json,
+    'movieC_echo-2_sbref': fmri_bold_image03_json,
+    'movieC_echo-3_sbref': fmri_bold_image03_json
+    }
+
+# Create an image03 for all data in upload
+
+# For each subject
+
+    # Grab subject level needed info
+
+    # For each session
+
+        # Grab session level needed info
+
+        # Find movie version
+
+        # Find experiment ID
+
+        # For each scan
+
+        # For functional image
+
+        # Specify the experiment ID
+
+        # Grab number of volumes to set 'image_extent4'
+
+        #
