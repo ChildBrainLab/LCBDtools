4baca3703208c8f867577d061056d620b4f686d3
Denny Schaedig <dennys@wustl.edu>
2025-05-06 19:06:34 -0600

    Upading structure, concentrating preprocessing and analysis scripts in dedeicated folders, removing files from old locations.

--- scripts/MRI/bsub/launch_sandbox.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CPUs=$1
-RAM=$2
-TMP=$3
-
-export LSF_DOCKER_VOLUMES="/storage1/fs1/perlmansusan/Active:/storage1/fs1/perlmansusan/Active $HOME:$HOME"
-
-bsub -G compute-perlmansusan -J $USER-sandbox -a 'docker(continuumio/anaconda3:latest)' -Is -n $CPUs -R "rusage[mem=${RAM}GB] select[mem>$RAM && tmp>$TMP]" /bin/bash 
