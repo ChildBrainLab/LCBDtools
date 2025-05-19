8bef34bc5cd4d72ecc75f8e5dab29fa44dda076f
Denny Schaedig <dennys@wustl.edu>
2025-05-19 10:40:54 -0600

    Adding hrfunc bsub commands for running deconvolution on IBM spectrum

---
+++ scripts/NIRS/bsub/care_deconv_synchrony_bsub.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+#BSUB -G compute-perlmansusan
+#BSUB -q general
+#BSUB -m general
+#BSUB -a 'docker(continuumio/anaconda3)'
+#BSUB -n 60
+#BSUB -R 'rusage[mem=80GB] select[mem>80000 && tmp>250]'
+
+conda init 
+source /home/dennys/.bashrc
+conda activate lcbd-env
+
+python3 /storage1/fs1/perlmansusan/Active/moochie/github/LCBDtools/scripts/NIRS/hrfunc/CARE_synchrony_deconv.py 
