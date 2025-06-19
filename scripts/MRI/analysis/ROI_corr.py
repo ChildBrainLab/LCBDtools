ac4053df09c3e9dfa8626020fb9c6cbfcf8b4195
Denny Schaedig <dennys@wustl.edu>
2025-06-18 23:45:20 -0600

    Updated ROI correlation scheme to only correlated subjects to group ROI activity, instead of the previous Monte Carlo subject-subject permutation

--- scripts/MRI/analysis/ROI_corr.py
+++ scripts/MRI/analysis/ROI_corr.py
@@ -5,6 +5,7 @@ from nilearn import datasets
 from nilearn.image import math_img, load_img, resample_to_img
 from nilearn.plotting import plot_roi
 from nilearn.masking import apply_mask
+from scipy.stats import zscore
 import numpy as np
 
 # Define runtime variables defining where data is and where to output results
@@ -97,44 +98,30 @@ for movie in movies: # For each movie
     subjects = [subject.split('/')[-4].split('sub-')[-1] for subject in subject_files] # Grab the subject ID from the file path
     subjects = [subject for subject in subjects if subject[-5:] != '_copy'] # Remove empty strings
 
-    dev_corr_matrix = np.zeros((len(ROIs.keys()), len(subjects), 2)) # Store subject-subject correlations for each ROI
     sub_dev_corr = {roi : {} for roi in ROIs.keys()} # Store subject-subject correlations for each ROI
 
+    # Grab all subject files and shuffle
+    subject_files = glob(f"{input_dir}sub-*/ses-0/func/sub-*_ses-0_task-movie{movie}_space-MNI152NLin2009cAsym_res-2_desc-preproc_bold_7mm_smoothed.nii") # Grab all subjects for movie
+    random.shuffle(subject_files)
+
     for roi in roi_list: # Iterate through ROIs
-        print(f"movie{movie}-{roi}")
+        print(f"movie{movie}-{roi}") # Construct ROI name
         roi_name = '-'.join(roi.split(' '))
-        
-        # Grab the ROI movies pre-calculated average and standard deviation
-        if os.path.exists(f"{output_dir}movie{movie}_{roi_name}_average_timecourse.npy") == False and os.path.exists(f"{output_dir}movie{movie}_{roi_name}_std_timecourse.npy") == False:
-            print(f"movie{movie}-{roi_name} average and standard deviation not calculated yet")
-            continue
-
-        roi_avg = np.load(f"{output_dir}{subject}/movie{movie}_{roi_name}_group_median_timecourse.npy")
-        roi_std = np.load(f"{output_dir}{subject}/movie{movie}_{roi_name}_group_std_timecourse.npy")
-
-        masks = ROIs[roi]['masks'] # Grab the ROI mask
-
-        subject_files = glob(f"{input_dir}sub-*/ses-0/func/sub-*_ses-0_task-movie{movie}_space-MNI152NLin2009cAsym_res-2_desc-preproc_bold_7mm_smoothed.nii") # Grab all subjects for movie
-        random.shuffle(subject_files)
 
         for subject_file in subject_files:
             # Grab subject directory
             subject_dir = subject_file.split('/')[-4]
-            subject = subject_dir
+            subject = subject_dir[-5:]
 
-            # Create name for mask
-            roi_dev_filename = f"{deviation_dir}{subject_dir}/ses-0/{subject_dir}_ses-0_roi-{roi_name}_deviation.npy"
-
-            roi_avg = np.load(f"{output_dir}movie{movie}_{roi_name}_average_timecourse.npy")
-            roi_std = np.load(f"{output_dir}movie{movie}_{roi_name}_std_timecourse.npy")
-
-            # Check if file exists and remove if needed
-            if os.path.exists(roi_dev_filename) and _overwrite == False:
-                print(f"Already calculated, skipping {roi_dev_filename}...")
+            # Skip if the folder is a copy
+            if subject == '_copy':
                 continue
-            elif os.path.exists(roi_dev_filename) and _overwrite == True:
-                print(f"Recalculating {roi_dev_filename}...")
-                os.remove(roi_dev_filename)
+            
+            # Load the ROI average and standard deviation for the subject
+            roi_avg = np.load(f"{output_dir}{subject}/movie{movie}_{roi_name}_group_median_timecourse.npy")
+            roi_std = np.load(f"{output_dir}{subject}/movie{movie}_{roi_name}_group_std_timecourse.npy")
+
+            masks = ROIs[roi]['masks'] # Grab the ROI mask
 
             # Load their data
             image = nib.load(subject_file)
@@ -143,31 +130,31 @@ for movie in movies: # For each movie
             for mask in masks:# Iterate through masks
                 print(f"Calculating {subject_file} correspondend from ROI {roi_name}...")
 
-                # Skip if average already calculated
-                if os.path.exists(roi_dev_filename) and _overwrite == False:
-                    print(f"Already calculated, skipping {roi_dev_filename}...")
-                    continue
-                elif os.path.exists(roi_dev_filename) and _overwrite == True:
-                    os.remove(roi_dev_filename)
-
                 # Resample mask to the subjects space
                 resampled_mask = nilearn.image.resample_to_img(mask, image, interpolation = 'nearest')
 
                 # Apply mask to get (n_timepoints x n_voxels)
                 masked_data = nilearn.masking.apply_mask(image, resampled_mask)
 
+                print(f"{subject_file} - {roi} - masked shape: {masked_data.shape}, nanmean: {np.nanmean(masked_data)}")
+
                 # Append to mask - use median to instead of mean to remove outliers like CSF and motion
-                mask_rois.append(np.median(masked_data, axis=1))
+                mask_rois.append(np.nanmean(masked_data, axis=1))
 
             # Calculate ROI average
             mask_rois = np.vstack(mask_rois)
             subject_roi = np.nanmean(mask_rois, axis = 0)
-            subject_abs = np.abs(subject_roi - roi_avg[:len(subject_roi)])
+            #subject_abs = np.abs(subject_roi - roi_avg[:len(subject_roi)])
             
             # Shorten the group ROI to be the size of the subjects scan
-            short_roi_std = roi_avg[:subject_roi.shape[0]]
+            short_roi_mean = roi_avg[:subject_roi.shape[0]]
 
-            results = scipy.stats.spearmanr(subject_abs, short_roi_std) # Correlate first subject with second subject
+            # Z score
+            subject_roi_z = zscore(subject_roi)
+            short_roi_mean_z = zscore(short_roi_mean)
+            results = scipy.stats.spearmanr(subject_roi_z, short_roi_mean_z)
+
+            results = scipy.stats.spearmanr(subject_roi, short_roi_mean) # Correlate first subject with second subject
             r_value = results.statistic # Grab r value
             p_value = results.pvalue # Grab p value
             print(f"{roi} vs {subject}: {r_value} ({p_value})")
@@ -177,18 +164,18 @@ for movie in movies: # For each movie
 
             
     # Save csv of subject-subject correlation for this movie
-    for roi_ind, roi in enumerate(ROIs.keys()): # Iterate through ROIs
-        for sub_ind, subject in enumerate(subjects):
-            csv_r_contents.append([subject, roi, movie] + [sub_dev_corr[roi][subject][0] for roi in ROIs.keys()])
-            csv_p_contents.append([subject, roi, movie] + [sub_dev_corr[roi][subject][1] for roi in ROIs.keys()])
+    for subject in subjects:
+        r_vals = [sub_dev_corr[roi][subject][0] for roi in ROIs.keys()]
+        p_vals = [sub_dev_corr[roi][subject][1] for roi in ROIs.keys()]
+        csv_r_contents.append([subject, movie] + r_vals)
+        csv_p_contents.append([subject, movie] + p_vals)
 
-header = ['Subject', 'ROI' 'Movie'] + [roi for roi in ROIs.keys()]
+header = ['Subject', 'Movie'] + [roi for roi in ROIs.keys()]
 with open(f"{deviation_dir}subject-ROI_correlation.csv", 'w') as f:
     writer = csv.writer(f)
     writer.writerow(header)
     writer.writerows(csv_r_contents)
 
-header = ['Subject', 'ROI', 'Movie'] + [roi for roi in ROIs.keys()]
 with open(f"{deviation_dir}subject-ROI_correlation_p-values.csv", 'w') as f:
     writer = csv.writer(f)
     writer.writerow(header)
