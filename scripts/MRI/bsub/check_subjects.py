bb80eecc2f796219f6f55c95f1f24e8de28e9567
Denny Schaedig <dennys@wustl.edu>
2025-09-30 13:09:11 -0600

    Adjusting status reporter to alert of single sessions that need to be preprocessed through our various pipelines

--- scripts/MRI/bsub/check_subjects.py
+++ scripts/MRI/bsub/check_subjects.py
@@ -2,7 +2,7 @@ import os
 from glob import glob
 
 
-def check_subjects(bids_folder = None, trad_fmriprep_folder = None, ME_fmriprep_folder = None, tedana_folder = None, xcp_folder = None):
+def check_subjects(session = None, output_space = "MNI152NLin6Asym", bids_folder = None, trad_fmriprep_folder = None, ME_fmriprep_folder = None, tedana_folder = None, xcp_folder = None):
 
     exclude = ['sub-50503', 'sub-50532', 'sub-50581', 'sub-50571', 'sub-50551']
 
@@ -18,11 +18,19 @@ def check_subjects(bids_folder = None, trad_fmriprep_folder = None, ME_fmriprep_
         xcp_folder = '/storage1/fs1/perlmansusan/Active/moochie/analysis/CARE/ME_XCP_khalil_data/'
 
 
-    bids_subjects = next(os.walk(bids_folder))[1]
-    trad_fmriprep_subjects = next(os.walk(trad_fmriprep_folder))[1]
-    ME_fmriprep_subjects = next(os.walk(ME_fmriprep_folder))[1]
-    tedana_subjects = next(os.walk(tedana_folder))[1]
-    xcp_subjects = next(os.walk(xcp_folder))[1]
+    bids_sessions = glob(f"{bids_folder}sub-*/ses-{session}")
+    trad_fmriprep_sessions = glob(f"{trad_fmriprep_folder}sub-*/ses-{session}")
+    ME_fmriprep_sessions = glob(f"{ME_fmriprep_folder}sub-*/ses-{session}")
+    tedana_sessions = glob(f"{tedana_folder}sub-*/ses-{session}")
+    xcp_sessions = glob(f"{xcp_folder}sub-*/ses-{session}")
+
+    bids_subjects = [folder.split("/")[-2] for folder in bids_sessions]
+    trad_fmriprep_subjects = [folder.split("/")[-2] for folder in trad_fmriprep_sessions]
+    ME_fmriprep_subjects = [folder.split("/")[-2] for folder in ME_fmriprep_sessions]
+    tedana_subjects = [folder.split("/")[-2] for folder in tedana_sessions]
+    xcp_subjects = [folder.split("/")[-2] for folder in xcp_sessions]
+
+    print(f" -- Session {session} -- ")
 
     print(f"BIDSified subjects: {len(bids_subjects)}")
     print(f'ME fMRIPrep Processed Subjects: {len(ME_fmriprep_subjects)}')
@@ -33,21 +41,19 @@ def check_subjects(bids_folder = None, trad_fmriprep_folder = None, ME_fmriprep_
 
     print('\n\nAvailable subjects to run traditional fMRIPrep on...')
     for subject in sorted(bids_subjects):
-        if subject[:4] != 'sub-': continue
-        if len(glob(f'/storage1/fs1/perlmansusan/Active/moochie/analysis/CARE/MRI_data/derivatives/fmriprep/{subject}/**/*MNI152NLin6Asym*.nii')) > 0: continue
+        if len(glob(f'/storage1/fs1/perlmansusan/Active/moochie/analysis/CARE/MRI_data/derivatives/fmriprep/sub-{subject}/ses-{session}/**/*{output_space}*.nii')) > 0:
+            continue
         if subject in trad_fmriprep_subjects or subject in exclude: continue
         print(subject.split('-')[1])
 
     print('\n\nAvailable subjects to run ME fMRIPrep on...')
-    for subject in sorted(bids_subjects):
-        if subject[:4] != 'sub-': continue
+    for subject in sorted(trad_fmriprep_subjects):
         if subject in ME_fmriprep_subjects or subject in exclude: continue
         print(subject.split('-')[1])
 
     print('\n\nAvailable subjects to run Tedana on...')
     for subject in sorted(ME_fmriprep_subjects):
-        if subject[:4] != 'sub-' or subject in exclude: continue
-        if subject in tedana_subjects: continue
+        if subject in tedana_subjects or subject in exclude: continue
         print(subject.split('-')[1])
 
     print('\n\nAvailable subjects to run XCP-D on...')
@@ -58,5 +64,6 @@ def check_subjects(bids_folder = None, trad_fmriprep_folder = None, ME_fmriprep_
 
 
 if __name__ == "__main__":
-    check_subjects()
+    for ses in [0, 1, 2]:
+        check_subjects(ses)
 
